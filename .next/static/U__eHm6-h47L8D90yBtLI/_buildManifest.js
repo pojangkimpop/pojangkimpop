967638fb8191153aece71a91f2d8803c07358829
pojangkimpop <4813_y00n@naver.com>
2024-08-25 20:15:32 +0900

    commit message

--- .next/static/U__eHm6-h47L8D90yBtLI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/49-217b026fa34f8fc3.js","static/chunks/pages/index-4fca206e1d51bd9b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- next.config.js
+++ next.config.js
@@ -2,9 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    unoptimized: true,
     domains: ['https://pojangkimpop.github.io/'], // 사용할 도메인 설정
   },
   assetPrefix: '/my-app/', // 배포 경로 설정
+  reactStrictMode: true,
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md/,
