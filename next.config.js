52bdb5242495d507dd85cf8f34640bfc2e7d69f5
pojangkimpop <4813_y00n@naver.com>
2024-08-25 20:07:03 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -1,15 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['https://pojangkimpop.github.io/'], // 사용할 도메인 설정
+  },
+  assetPrefix: '/my-app/', // 배포 경로 설정
   webpack: (config) => {
     config.module.rules.push({
       test: /\.md/,
-      use: "raw-loader",
+      use: 'raw-loader',
     });
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
-      use: ["@svgr/webpack"],
+      use: ['@svgr/webpack'],
     });
     return config;
   },
