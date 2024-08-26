0c9e868ec1a0aaf2b9c881b8d5bba6b232aba495
pojangkimpop <4813_y00n@naver.com>
2024-08-26 11:53:28 +0900

    commit

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     unoptimized: true, // 이미지 최적화 비활성화
     domains: ['pojangkimpop.github.io'], // 사용할 도메인 설정 (도메인 이름만 입력)
   },
-  assetPrefix: '/my-app/', // 배포 경로 설정
-  basePath: '/my-app', // 애플리케이션의 기본 경로를 설정합니다.
+  assetPrefix: '', // 배포 경로 설정
+  basePath: '', // 애플리케이션의 기본 경로를 설정합니다.
   webpack: (config) => {
     // Markdown 파일을 raw-loader를 사용하여 처리합니다.
     config.module.rules.push({
